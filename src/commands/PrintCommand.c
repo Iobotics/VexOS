2879c5da6eb2f3d472b4359f265415f3c70027c1
Jeff Malins <jmalins@iolani.org>
2012-12-13 16:02:11 -1000

    Added copyright and license statement to all files that were adapated from WPILibJ code. Credit must be given to the original authors for inspiring and seeding the initial parts of this port.

--- src/commands/PrintCommand.c
+++ src/commands/PrintCommand.c
@@ -18,6 +18,17 @@
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.  
 //
+//  --------------------------------------------------------------------------
+//
+//  Some algorithms, structures and code organization used by this file have
+//  been adapted from the WPILibJ software package. Those portions are:
+//
+//  Copyright (c) FIRST 2008-2012. All rights reserved.
+//  
+//  WPILibJ elements are distributed in this program in accordance to their 
+//  original FIRST BSD License, available as 'BSD_License_for_WPILib_code.txt' 
+//  present in this distribution.
+//
 
 #include "CommandClass.h"
 #include "Error.h"
